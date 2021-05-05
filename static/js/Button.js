ee18b27dcf76fa1f0ac4c9546acaaff7dc865e09
Daniel Fonseka <dlfonseka@utexas.edu>
2021-05-05 15:03:40 -0500

    Removes old react files

--- static/js/Button.js
+++
@@ -1,9 +0,0 @@
-function Button({text, onClick, color, id, type}) {
-    return (
-        <button id={id} onClick={onClick} style={{backgroundColor:color}} className='btn' type={type}>
-            {text}
-        </button>
-    )
-}
-
-export default Button
--- static/js/Entry.js
+++
@@ -1,9 +0,0 @@
-function Entry({entry}) {
-    return (
-        <div className='entry'>
-            <p><strong>{entry.tool}</strong> {entry.detail}</p>
-        </div>
-    )
-}
-
-export default Entry
